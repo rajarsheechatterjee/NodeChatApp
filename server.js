376ade602c75303cfca2d403d33b196d2fc9a369
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2020-07-07 09:16:29 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+
+app.use
+
+
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
